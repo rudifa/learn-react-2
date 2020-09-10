d43d7b627d72314fc7750b32f22ac28e52ad243a
Rudolf Farkas <rudi.farkas@gmail.com>
2020-09-10 23:45:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -40,17 +40,42 @@ const posts = [
 class Post extends React.Component {
   state = {
     title: "Post One",
-    body: "This is the first post"
+    content: ["This is the first phrase", "Second phrase"]
   };
   render() {
     return (
       <div>
         <h3>{this.state.title}</h3>
-        <p>{this.state.body}</p>
+        <ul>
+          {this.state.content.map((topic) => (
+            <li>{topic}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
+
+//{ this.searchResultsComponents.map((it, index) => <it key={index} />) }
+
+function RenderItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+
+function RenderList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((renderedItem, index) => (
+    <RenderItem key={index} value={renderedItem} />
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+const numbers = ["123", 2, 3, 4, 5, 5];
+ReactDOM.render(
+  <RenderList numbers={numbers} />,
+  document.getElementById("root")
+);
 
 //ReactDOM.render(<Blog posts={posts} />, document.getElementById("root"));
 export default function App() {
